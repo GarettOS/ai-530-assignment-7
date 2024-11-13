e6588adadb3e39c16fa93453eb81c79e84cf4f25
garett102 <garett2777@gmail.com>
2024-11-13 10:21:32 -0800

    freq dict made

--- build_model.py
+++ build_model.py
@@ -22,12 +22,38 @@ def build_model(memory, prediction_window):
     # Loop through each word to find the sequences of that word
     for i in range(len(filtered_words) - prediction_window):
         # Find the sequences of length memory
-        word_sequences.append([filtered_words[i:i + memory]])
+        word_sequences.append(filtered_words[i:i + memory])
         # Find the word sequence of length prediction_window that follows it 
-        prediction_word_sequences.append([filtered_words[i + memory: i + memory + prediction_window]])
+        prediction_word_sequences.append(filtered_words[i + memory: i + memory + prediction_window])
 
-    print(word_sequences)
-    print(prediction_word_sequences)
+    # Make a freq dictionary to count how often sequences appear in the text
+    # freq_dict = {
+    #     sequence {
+    #         prediction_sequence: count
+    #          .
+    #          .
+    #     }
+    #     sequence2 {
+    #         prediction_sequence: count
+    #          .
+    #          .
+    #     }
+    # will have to convert sequence lists to tuples to store in dict
+
+    freq_dict = {}
+    for i in range(len(word_sequences)):
+        # Check if sequence currently exists in freq dict
+        if tuple(word_sequences[i]) not in freq_dict:
+            freq_dict[tuple(word_sequences[i])] = {} # Initialize a dict for the predictions and their couns
+
+        # Check if the prediction that follows the sequence is in the dict
+        if tuple(prediction_word_sequences[i]) not in freq_dict[tuple(word_sequences[i])]:
+            freq_dict[tuple(word_sequences[i])][tuple(prediction_word_sequences[i])] = 0 # initalize the count to 0 if our first time seeing this prediction
+        
+        # Increment the freq of the prediction sequence following this word sequence
+        freq_dict[tuple(word_sequences[i])][tuple(prediction_word_sequences[i])] += 1
+        
+    
 
     return
 
